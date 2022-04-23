16edee2159f542b40a215d2607cc61ca5792b5a8
lhmuana <edd_vai@yahoo.co.in>
2022-04-23 21:08:35 +0530

    added <a> tag and opened link in new tab

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -16,7 +16,9 @@ function renderLeads(){
 let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
     // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItems += "<li>" + myLeads[i] + "</li>" ;
+     // Wrap the lead in an anchor tag (<a>) inside the <li>
+     // Can you make the link open in a new tab?
+    listItems += "<li><a href='"+ myLeads[i] +"'target='_blank'>" + myLeads[i] +"</a></li>" ;
 }
 // 3. Render the listItems inside the unordered list using ulEl.innerHTML
 ulEl.innerHTML = listItems;
