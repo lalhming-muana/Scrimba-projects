2ffb676c66c996ba2f19d5d6c778120fcc9dc7a6
lhmuana <edd_vai@yahoo.co.in>
2022-04-26 20:24:42 +0530

    template string added to the links

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -18,11 +18,26 @@ for (let i = 0; i < myLeads.length; i++) {
     // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
      // Wrap the lead in an anchor tag (<a>) inside the <li>
      // Can you make the link open in a new tab?
-    listItems += "<li><a href='"+ myLeads[i] +"'target='_blank'>" + myLeads[i] +"</a></li>" ;
+    // listItems += "<li><a href='"+ myLeads[i] +"'target='_blank'>" + myLeads[i] +"</a></li>" ;
+    listItems += `<li>
+                    <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+                </li>` ;
 }
 // 3. Render the listItems inside the unordered list using ulEl.innerHTML
 ulEl.innerHTML = listItems;
 }
 
 
+
+// template strings/literals
+const recipient = "James"
+const sender = "Alvin"
+
+// Refactor the email string to use template strings
+const email = `Hey ${recipient}!
+               How is it going? 
+               Cheers, 
+               ${sender}`
+
+console.log(email)
 
